502d6cc9b0eb7d4977594d24cabd3bc19101f877
Masayuki Uehara <uehara.masayuki@dena.jp>
2014-10-06 19:44:54 +0900

    [from now] 2014/10/06 19:44:54
     LiveHouse.xcodeproj/project.pbxproj  |  6 ++++
     LiveHouse/Base.lproj/Main.storyboard | 27 ++++++++++++++++-
     LiveHouse/LHHomeViewController.h     | 13 ++++++++
     LiveHouse/LHHomeViewController.m     | 57 ++++++++++++++++++++++++++++++++++++
     4 files changed, 102 insertions(+), 1 deletion(-)

---
+++ LiveHouse/LHHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  LHHomeViewController.h
+//  LiveHouse
+//
+//  Created by Masayuki Uehara on 10/6/14.
+//  Copyright (c) 2014 LIVEHOUSE inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LHHomeViewController : UIViewController
+
+@end
---
+++ LiveHouse/LHHomeViewController.m
@@ -0,0 +1,57 @@
+//
+//  LHHomeViewController.m
+//  LiveHouse
+//
+//  Created by Masayuki Uehara on 10/6/14.
+//  Copyright (c) 2014 LIVEHOUSE inc. All rights reserved.
+//
+
+#import "LHHomeViewController.h"
+
+@interface LHHomeViewController () <UIWebViewDelegate>
+
+@end
+
+@implementation LHHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    UIWebView *wv = [[UIWebView alloc] init];
+    wv.delegate = self;
+    wv.frame = self.view.frame;
+    wv.scalesPageToFit = YES;
+    [self.view addSubview:wv];
+    NSURL *url = [NSURL URLWithString:@"http://app.lvhs.jp/app"];
+    NSURLRequest *req = [NSURLRequest requestWithURL:url];
+    [wv loadRequest:req];
+
+}
+
+// ページ読込開始時にインジケータをくるくるさせる
+-(void)webViewDidStartLoad:(UIWebView*)webView{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+}
+
+// ページ読込完了時にインジケータを非表示にする
+-(void)webViewDidFinishLoad:(UIWebView*)webView{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
